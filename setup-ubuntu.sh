669498c168f833dd69ab53b66d327bd15a30ffb4
Daniel Rodriguez Gil <daniel@dpstudios.es>
2020-05-02 15:40:02 +0000

    Add docker-compose to ubuntu setup

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -20,6 +20,9 @@ sudo add-apt-repository \
 sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
+sudo curl -L \
+  "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname-s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
 
 # GITCONFIG
 git config --global user.name "Daniel Rodriguez Gil"
@@ -28,14 +31,14 @@ git config --global user.email "daniel@dpstudios.es"
 # TOOLS
 sudo apt install -y zip bat silversearcher-ag direnv
 
-# JAVA
+# Java
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 13.0.2.hs-adpt
 sdk install maven
 sdk install gradle
 
-# NVM
+# Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -52,6 +55,9 @@ npm install -g prettier \
 
 # Go
 sudo apt install golang
+
+# PHP
+sudo apt install -y php php-soap php-mysql php-gd php-xdebug php-xml php-curl composer
 
 # DOTFILES
 git clone git@github.com:danybmx/dotfiles-workspace.git ~/.dotfiles
