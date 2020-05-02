24157e66a03ed8c5a9466682a0642b3db0ec40ac
Daniel Rodriguez Gil <daniel@dpstudios.es>
2020-05-02 15:41:01 +0000

    change docker-compose permissions

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -23,6 +23,7 @@ sudo usermod -aG docker $USER
 sudo curl -L \
   "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname-s)-$(uname -m)" \
   -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 
 # GITCONFIG
 git config --global user.name "Daniel Rodriguez Gil"
