1ace45a5d42fb27850ed5b94aaf83c13734c2bdc
Daniel Rodriguez Gil <daniel@dpstudios.es>
2020-05-02 15:01:10 +0200

    Add ZSH_CUSTOM to env on setup

--- install-zsh.sh
+++ install-zsh.sh
@@ -16,6 +16,7 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing spaceship prompt
+export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
