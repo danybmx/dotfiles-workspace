cd42db5a130ff9b73673fc4968d24bd022e6927f
Daniel Rodriguez Gil <daniel@dpstudios.es>
2020-05-10 14:27:03 +0200

    Stop setup execution if something fails

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,46 +1,54 @@
+#!/bin/bash 
+set -e
+
 # UPGRADE
 sudo apt update -y
 sudo apt upgrade -y
 
 # EDITOR
-sudo apt install -y neovim python3-neovim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 61
-sudo update-alternatives --set vi /usr/bin/nvim
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --set vim /usr/bin/nvim
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --set editor /usr/bin/nvim
+if ! [ -x "$(command -v nvim)" ]; then
+  sudo apt install -y neovim python3-neovim
+  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 61
+  sudo update-alternatives --set vi /usr/bin/nvim
+  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+  sudo update-alternatives --set vim /usr/bin/nvim
+  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+  sudo update-alternatives --set editor /usr/bin/nvim
+fi
 
 # DOCKER
-sudo apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   bionic \
-   stable"
-sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
-sudo curl -L \
-  "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname-s)-$(uname -m)" \
-  -o /usr/local/bin/docker-compose
-sudo chmod a+x /usr/local/bin/docker-compose
+if ! [ -x "$(command -v docker)" ]; then
+  sudo apt install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     bionic \
+     stable"
+  sudo apt update -y
+  sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+  sudo curl -L \
+    "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname-s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+  sudo chmod a+x /usr/local/bin/docker-compose
+fi
 
 # GITCONFIG
 git config --global user.name "Daniel Rodriguez Gil"
 git config --global user.email "daniel@dpstudios.es"
 
 # TOOLS
-sudo apt install -y zip bat silversearcher-ag direnv tmuxinator
+sudo apt install -y zip silversearcher-ag direnv tmuxinator
 
 # Java
 curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+echo $HOME/.sdkman/bin/sdkman-init.sh
+source $HOME/.sdkman/bin/sdkman-init.sh
 sdk install java 13.0.2.hs-adpt
 sdk install maven
 sdk install gradle
